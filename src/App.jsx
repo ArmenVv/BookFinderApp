394705b1b76c4d2903b7200844620afb46274815
ArmenVv <armenvardanian2024@gmail.com>
2025-04-15 21:00:35 +0400

    Added Light/Dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import Search from './components/search'
-import './App.css'
+  import { useState } from 'react'
+  import Search from './components/search'
+  import './App.css'
+  import ToggleMode from "./components/togglemode";
 
-function App() {
-  return (
-    <div className='div-container'>
-      <Search/>
-    </div>
-  )
-}
+  function App() {
+    const [toggleBg, setToggleBg] = useState(true);
+    const handleToggle = () => {
+      setToggleBg(!toggleBg)
+    }
+    const bgStyle = {
+      backgroundColor: `${toggleBg ? "white" : "#041533"}`,
+      backgroundSize: "cover",
+      minHeight: "100vh",
+      color: `${toggleBg ? "black" : "white"}`,
+    };
+    return (
+      <>
+      <div className='div-container' style={bgStyle}>
+        <Search/>
+        <ToggleMode toggleBg = {toggleBg} handleToggle = {handleToggle}/>
+      </div>
+        </>
+    )
+  }
 
-export default App
+  export default App
---
+++ src/components/togglemode.jsx
@@ -0,0 +1,9 @@
+import { use, useState } from "react"
+import "./togglemode.css"
+export default function ToggleMode({toggleBg, handleToggle}){
+    return (
+        <div className="button-container" onClick={handleToggle} style={{backgroundColor : toggleBg ? "/src/assets/2.jpg":"/src/assets/1.jpg"}}>
+          <img src={toggleBg?"src/assets/sun.png":"src/assets/moon.png"}/>
+        </div>
+    )
+}
